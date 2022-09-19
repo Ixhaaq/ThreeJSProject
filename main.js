6cc3c3c510c330e636d3e097b7b80adafad95f34
Ixhaaq <55302887+Ixhaaq@users.noreply.github.com>
2022-09-19 23:35:21 +0100

    Add background texture, moon and moon texture, randomly generated stars

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ import "./style.css"
 import * as THREE from "three";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { SphereGeometry } from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
@@ -33,6 +34,35 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera,renderer.domElement);
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.1);
+  const material = new THREE.MeshStandardMaterial( {color:0xffffff})
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(400).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
+//moon
+
+const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({
+    map:moonTexture,
+    normalMap: normalTexture
+  })
+);
+scene.add(moon)
+
 // renderer.render( scene, camera);
 
 function animate( ) {
@@ -42,14 +72,8 @@ function animate( ) {
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-
-
   controls.update()
-
-
-
-  renderer.render( scene, camera);
-
+  renderer.render(scene, camera);
 }
 
 animate()
